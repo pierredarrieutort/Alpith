31fdd793d10208c16be5c157196a1a61c28d9292
Alex Dean <alex.dean9g@gmail.com>
2020-04-09 17:33:10 +0200

    movie entity and easy admin

---
+++ src/Entity/Movie.php
@@ -0,0 +1,176 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\MovieRepository")
+ */
+class Movie
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $released;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $rating;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $thumbnail;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $runtime;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $director;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $actors;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $imdbID;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getReleased(): ?\DateTimeInterface
+    {
+        return $this->released;
+    }
+
+    public function setReleased(\DateTimeInterface $released): self
+    {
+        $this->released = $released;
+
+        return $this;
+    }
+
+    public function getRating(): ?int
+    {
+        return $this->rating;
+    }
+
+    public function setRating(?int $rating): self
+    {
+        $this->rating = $rating;
+
+        return $this;
+    }
+
+    public function getThumbnail(): ?string
+    {
+        return $this->thumbnail;
+    }
+
+    public function setThumbnail(?string $thumbnail): self
+    {
+        $this->thumbnail = $thumbnail;
+
+        return $this;
+    }
+
+    public function getRuntime(): ?int
+    {
+        return $this->runtime;
+    }
+
+    public function setRuntime(int $runtime): self
+    {
+        $this->runtime = $runtime;
+
+        return $this;
+    }
+
+    public function getDirector(): ?string
+    {
+        return $this->director;
+    }
+
+    public function setDirector(string $director): self
+    {
+        $this->director = $director;
+
+        return $this;
+    }
+
+    public function getActors(): ?string
+    {
+        return $this->actors;
+    }
+
+    public function setActors(?string $actors): self
+    {
+        $this->actors = $actors;
+
+        return $this;
+    }
+
+    public function getImdbID(): ?string
+    {
+        return $this->imdbID;
+    }
+
+    public function setImdbID(?string $imdbID): self
+    {
+        $this->imdbID = $imdbID;
+
+        return $this;
+    }
+}
