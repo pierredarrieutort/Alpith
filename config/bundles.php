3dca9a42da0fc22078603c15c3fdfd3eb3b9c754
Pierre DARRIEUTORT <p.darrieutort@outlook.fr>
2020-04-09 19:54:14 +0200

    webpack properly setup

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,5 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
