dedfbae9b963235a2c2c1de370772c453242f6d7
Pierre DARRIEUTORT <p.darrieutort@outlook.fr>
2020-04-11 19:11:00 +0200

    force_seek working

--- assets/js/app.js
+++ assets/js/app.js
@@ -38,25 +38,31 @@ function iframe_init( player, i ) {
             mute: 1
         },
         events: {
-            'onReady': player_behavior_init,
+            'onReady': player_init_behavior,
             'onStateChange': state_listener
         }
     } )
 }
 
 function state_listener( { data, target } ) {
-    if ( /3/.test( data ) )
+    if ( /3/.test( data ) ) {
+        console.log( 'a' )
         trailer_limiter( target, false )
+    }
 
-    if ( /1/.test( data ) )
+    if ( /1/.test( data ) ) {
+        console.log( 'b' )
         trailer_limiter( target )
+    }
 
-    if ( /0|5/.test( data ) )
+    if ( /0|5/.test( data ) ) {
+        console.log( 'c' )
         poster_to_trailer( target )
+    }
 }
 
 let targets_warehouse = []
-function player_behavior_init( { target } ) {
+function player_init_behavior( { target } ) {
     trailer_limiter( target, false )
     targets_warehouse = [target, ...targets_warehouse]
 
@@ -68,7 +74,7 @@ function player_behavior_init( { target } ) {
 
 let hovered_target = false
 function poster_to_trailer( target ) {
-    console.log( 'poster_to_trailer', target.f.id )
+    console.log( 'poster_to_trailer', target.f.id.substr( target.f.id.length - 1 ) )
     hovered_target = true
     document.getElementById( 'trends-carousel' ).onmouseleave = () => trailer_to_poster( target )
     if ( hovered_target ) {
@@ -77,27 +83,30 @@ function poster_to_trailer( target ) {
     }
 }
 
-function trailer_to_poster( target ) {
-    console.log( 'trailer_to_poster', target.f.id )
+function trailer_to_poster( target, end_of_stream = false ) {
+    console.log( 'trailer_to_poster', target.f.id.substr( target.f.id.length - 1 ) )
     hovered_target = false
     target.f.style.opacity = 0
-    setTimeout( () => trailer_limiter( target, false ), 1000 )
+    if ( !end_of_stream )
+        setTimeout( () => trailer_limiter( target, false, true ), 1000 )
 }
 
-function trailer_limiter( target, recall = true ) {
-    console.log( 'trailer_limiter',  recall, hovered_target)
+function trailer_limiter( target, recall = true, force_seek ) {
+    console.log( 'trailer_limiter', target.f.id.substr( target.f.id.length - 1 ), recall, hovered_target )
     const
         START_TIME = 3,
         TRAILER_DURATION = 30 - START_TIME
 
-    if ( target.getCurrentTime() < START_TIME || target.getCurrentTime() >= TRAILER_DURATION ) {
+    if ( target.getCurrentTime() < START_TIME || target.getCurrentTime() >= TRAILER_DURATION || force_seek) {
         target.seekTo( START_TIME )
-        if ( !hovered_target ) {
-            recall = false
-            trailer_to_poster( target )
-        }
     }
-    if ( recall && hovered_target)
-        setTimeout( () => trailer_limiter( target, recall ), 1000 )
-    else target.pauseVideo()
+
+    if ( recall ) {
+        if ( hovered_target ) {
+            setTimeout( () => trailer_limiter( target, recall ), 1000 )
+        } else {
+            recall = false
+            trailer_to_poster( target, true )
+        }
+    } else target.pauseVideo()
 }
